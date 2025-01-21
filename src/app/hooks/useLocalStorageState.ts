c2a5a5f11e91d685abf02c3e89b295fae3a656fe
WONTAE\spald <wontae.gramin9@gmail.com>
2025-01-21 19:50:13 +0000

    [Fix] arrays do not have named properties, so the user of the hook does not know which variable is which type → Explicitly define the type of the return types

--- src/app/hooks/useLocalStorageState.ts
+++ src/app/hooks/useLocalStorageState.ts
@@ -1,8 +1,11 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, Dispatch, SetStateAction } from "react";
 
-export function useLocalStorageState(initialState: boolean, key: string) {
+export function useLocalStorageState(
+  initialState: boolean,
+  key: string
+): [boolean, Dispatch<SetStateAction<boolean>>] {
   // [Wontae] lazy state initialization
-  const [value, setValue] = useState(initialState);
+  const [value, setValue] = useState<boolean>(initialState);
 
   useEffect(() => {
     const storedValue = localStorage.getItem(key);
@@ -18,5 +21,9 @@ export function useLocalStorageState(initialState: boolean, key: string) {
     [value, key]
   );
 
+  // [Wontae] Since the goal is not to fix the names (to create a reusable HOOK),
+  // receiving [isDarkMode, setIsDarkMode] as an array in DarkModeProvider
+  // makes it unclear which one is which, as arrays do not have named properties.
+  // Solution: Explicitly define the order of the returned values of the hook.
   return [value, setValue];
 }
