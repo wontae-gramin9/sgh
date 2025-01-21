6728c9b343a25c2bec459f1f0cb7391bde3017ad
WONTAE\spald <wontae.gramin9@gmail.com>
2025-01-21 10:40:59 +0000

    [Update] Card component design update

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -39,17 +39,26 @@ function Card(props: { card: CardType }) {
   }
 
   return (
-    <div className="rounded-lg border border-purple-3 bg-white-1 p-6 shadow-lg">
+    <div className="rounded-lg border border-purple-3 bg-white-1 p-6 shadow-lg transition duration-500 hover:scale-1">
       <form onSubmit={handleSubmit}>
-        <p className="text-purple-2">Server Name: {name}</p>
-        <p className="text-orange-2">Game Name: {game}</p>
-        <p className="text-yellow-2">Player Count: {players}</p>
-        <Badge type={serverStatus === "online" ? "success" : "error"}>
-          {serverStatus === "online" ? "Online" : "Offline"}
-        </Badge>
-        <Button disabled={loading}>
-          {serverStatus === "online" ? "Stop" : "Start"} Server
-        </Button>
+        <div className="text-center">
+          <h2 className="text-xl font-semibold text-purple-2">{name}</h2>
+          <p>{game}</p>
+        </div>
+        <div className="mt-2 flex items-center justify-between">
+          <p>{players} players online</p>
+          <Badge type={serverStatus === "online" ? "success" : "error"}>
+            {serverStatus === "online" ? "Online" : "Offline"}
+          </Badge>
+        </div>
+        <div className="mt-4 flex items-center justify-center">
+          <Button
+            className={`rounded-lg px-4 py-2 font-medium text-white-1`}
+            disabled={loading}
+          >
+            {serverStatus === "online" ? "Stop" : "Start"} Server
+          </Button>
+        </div>
       </form>
     </div>
   );
