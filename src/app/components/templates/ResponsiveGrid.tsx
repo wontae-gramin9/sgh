ce56931f950757630376fb5d1b1f1bd49df53a3c
WONTAE\spald <wontae.gramin9@gmail.com>
2025-01-22 19:55:19 +0000

    [Update] grid-col responsive to width

--- src/app/components/templates/ResponsiveGrid.tsx
+++ src/app/components/templates/ResponsiveGrid.tsx
@@ -4,7 +4,7 @@ export default function ResponsiveGrid({
   children: React.ReactNode;
 }) {
   return (
-    <pre className="grid w-full grid-cols-3 gap-x-4 gap-y-8 rounded-lg bg-background-nm p-4">
+    <pre className="grid w-full grid-cols-[repeat(auto-fit,minmax(18rem,1fr))] gap-x-4 gap-y-8 rounded-lg bg-background-nm p-4">
       {children}
     </pre>
   );
